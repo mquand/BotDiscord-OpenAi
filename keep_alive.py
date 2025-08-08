3069fc6442faefa9b55ed5a55d01d27b114fb867
mquand <doanquan087@gmail.com>
2025-08-08 10:20:34 +0700

    3 commit

--- keep_alive.py
+++ keep_alive.py
@@ -9,8 +9,8 @@ def home():
     return "Bot đang chạy trên Render!"
 
 def run():
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='127.0.0.1', port=port)
+    port = int(os.environ.get("PORT", 10000))  # Render sẽ set PORT tự động
+    app.run(host='0.0.0.0', port=port)         # Quan trọng: host phải là 0.0.0.0
 
 def keep_alive():
     t = threading.Thread(target=run)
